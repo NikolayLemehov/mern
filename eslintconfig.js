f74f4536d5aa2b91c0da5ed0a5176cc74ec33da7
NikolayLiemiekhov <nicklemster@gmail.com>
2021-07-31 20:24:40 +0300

    Добавляет исключения в eslint

--- eslintconfig.js
+++ eslintconfig.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['htmlacademy/es6'],
   rules: {
     'no-console': 0,
+    'no-process-exit': 0,
     'comma-dangle': ['error', {
       'arrays': 'never',
       'objects': 'always-multiline',
