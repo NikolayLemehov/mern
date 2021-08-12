82248848040124b3e139590dcc549518b1e23b62
NikolayLiemiekhov <nicklemster@gmail.com>
2021-08-12 16:13:27 +0300

    Обучает страницу AuthPage обращаться к серверу

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,11 +1,19 @@
 import React, {useState} from 'react';
+import {useHttp} from '../hooks/http.hook';
 
 const AuthPage = () => {
+  const {loading, request} = useHttp()
   const [form, setForm] = useState({
     email: '', password: ''
   })
   const changeHandler = (evt) => {
     setForm({...form, [evt.target.name]: [evt.target.value]})
+  }
+  const registerHandler = async () => {
+    try {
+      const data  = await request('/api/auth/register', 'POST', {...form})
+      console.log('data', data)
+    } catch (e) {}
   }
 
   return (
@@ -39,8 +47,15 @@ const AuthPage = () => {
             </div>
           </div>
           <div className="card-action">
-            <button className="btn yellow darken-4">Войти</button>
-            <button className="btn grey lighten-1 black-text">Регистрация</button>
+            <button
+              className="btn yellow darken-4"
+              disabled={loading}
+            >Войти</button>
+            <button
+              className="btn grey lighten-1 black-text"
+              onClick={registerHandler}
+              disabled={loading}
+            >Регистрация</button>
           </div>
         </div>
       </div>
