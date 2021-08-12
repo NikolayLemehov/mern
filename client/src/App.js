a2d73b445deb702d28e45009f83eb06b15f26503
NikolayLiemiekhov <nicklemster@gmail.com>
2021-08-12 20:48:09 +0300

    Записывает в localStorage данные авторизации, добавляет context

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,23 @@ import 'materialize-css'
 import './index.css'
 import {useRoutes} from './routes';
 import {BrowserRouter} from 'react-router-dom';
+import {useAuth} from './hooks/auth.hook';
+import {AuthContext} from './context/AuthContext';
 
 const App = () => {
-  const routes = useRoutes(false)
+  const {token, login, logout, userId} = useAuth()
+  const isAuthenticated = !!token
+  const routes = useRoutes(isAuthenticated)
   return (
-    <BrowserRouter>
-      <div className="container">
-        {routes}
-      </div>
-    </BrowserRouter>
+    <AuthContext.Provider value={{
+      token, login, logout, userId, isAuthenticated
+    }}>
+      <BrowserRouter>
+        <div className="container">
+          {routes}
+        </div>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 };
 
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,8 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {useHttp} from '../hooks/http.hook';
 import {useMessage} from '../hooks/message.hook';
+import {AuthContext} from '../context/AuthContext';
 
 const AuthPage = () => {
+  const auth = useContext(AuthContext)
   const message = useMessage()
   const {loading, request, error, clearError} = useHttp()
   const [form, setForm] = useState({
@@ -24,7 +26,7 @@ const AuthPage = () => {
   const loginHandler = async () => {
     try {
       const data  = await request('/api/auth/login', 'POST', {...form})
-      message(data.message)
+      auth.login(data.token, data.userId)
     } catch (e) {}
   }
 
